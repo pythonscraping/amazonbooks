5565ecb85ee455c104751cbb8c82ec9b86f9fa0c
Brice <brice.jarmoszko@mail.mcgill.ca>
2016-01-25 00:08:31 -0500

    table for reviews

--- amazonbooks/tablesql.py
+++ amazonbooks/tablesql.py
@@ -1,36 +1,28 @@
 import psycopg2
 conn = psycopg2.connect("dbname=amazon user=amazon password=amazon host=127.0.0.1")
 cur = conn.cursor()
-cur.execute("""CREATE TABLE safelist (
+cur.execute("""CREATE TABLE reviews (
 id SERIAL PRIMARY KEY,
-asin text UNIQUE NOT NULL,
-url text NOT NULL,
-reason text NOT NULL,
-releasedate text NOT NULL,
+asin text REFERENCES safelist(asin),
+review text NOT NULL,
+title text NOT NULL,
+date text NOT NULL,
+amazonid text NOT NULL,
+reviewer text,
+reviewerurl text,
+format text,
+rating INT,
+helpful INT,
+total INT,
+verified boolean,
+vinevoice boolean,
+top10 boolean,
+top50 boolean,
+top100 boolean,
+top500 boolean,
+top1000 boolean,
+halloffame boolean,
+indexcount INT,
 scrapedate DATE
-);""")
-cur.execute("""CREATE TABLE books(
-id SERIAL PRIMARY KEY,
-url text NOT NULL,
-asin text UNIQUE NOT NULL,
-kindle real,
-hardcover real,
-paperback real,
-massmarketpaperback real,
-listprice real,
-publisher text,
-isbn10 text,
-isbn13 text,
-dimensions text,
-bestsellerrank INT,
-average real,
-haseditorialreview INT,
-allowpreview INT,
-scrapedate DATE
-);""")
-cur.execute(""" CREATE TABLE alsobought(
-id SERIAL PRIMARY KEY,
-asin text REFERENCES books(asin),
-alsoboughtasin text NOT NULL
 );""")
 conn.commit()
