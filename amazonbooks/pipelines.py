bdcbe00dca472f8928c2ec4a8231c10ed7b462a4
Brice <brice.jarmoszko@mail.mcgill.ca>
2016-01-24 21:42:12 -0500

    database test

--- amazonbooks/pipelines.py
+++ amazonbooks/pipelines.py
@@ -21,8 +21,11 @@ class psqlpipeline(object):
             try :
                 conn = psycopg2.connect("dbname=amazon user=amazon password=amazon host=127.0.0.1")
                 cur = conn.cursor()
+                kindle = float(item['kindle'])
+                hardcover = float(item['hardcover'])
+                paperback = float(item['paperback'])
                 SQL = "INSERT INTO books (url,asin,kindle,hardcover,paperback) VALUES (%s,%s);"
-                data = (item['url'], item['asin'],item['kindle'],item['hardcover'],item['paperback'])
+                data = (item['url'], item['asin'],kindle,hardcover,paperback)
                 cur.execute (SQL,data)
                 conn.commit()
             except:
