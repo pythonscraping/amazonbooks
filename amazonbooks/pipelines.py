93cd4dfbe3b8455ca31e4a26cc5f127634d2b09a
Brice <brice.jarmoszko@mail.mcgill.ca>
2016-01-24 21:42:52 -0500

    database test

--- amazonbooks/pipelines.py
+++ amazonbooks/pipelines.py
@@ -18,18 +18,18 @@ class psqlpipeline(object):
     def process_item(self, item, spider):
         if (spider.name == "amazons") : #Only execute it for the amazons pipeline
             #print spider.name
-            try :
-                conn = psycopg2.connect("dbname=amazon user=amazon password=amazon host=127.0.0.1")
-                cur = conn.cursor()
-                kindle = float(item['kindle'])
-                hardcover = float(item['hardcover'])
-                paperback = float(item['paperback'])
-                SQL = "INSERT INTO books (url,asin,kindle,hardcover,paperback) VALUES (%s,%s);"
-                data = (item['url'], item['asin'],kindle,hardcover,paperback)
-                cur.execute (SQL,data)
-                conn.commit()
-            except:
-                print "Database error"
+            #try :
+            conn = psycopg2.connect("dbname=amazon user=amazon password=amazon host=127.0.0.1")
+            cur = conn.cursor()
+            kindle = float(item['kindle'])
+            hardcover = float(item['hardcover'])
+            paperback = float(item['paperback'])
+            SQL = "INSERT INTO books (url,asin,kindle,hardcover,paperback) VALUES (%s,%s);"
+            data = (item['url'], item['asin'],kindle,hardcover,paperback)
+            cur.execute (SQL,data)
+            conn.commit()
+            #except:
+            #    print "Database error"
             return item
         else:
             print item['url']
