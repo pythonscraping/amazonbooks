a6b7bbcd0e7f1bd3fd1dd53ae69cc6e500272cc0
Brice <brice.jarmoszko@mail.mcgill.ca>
2016-01-24 21:29:07 -0500

    database test

--- amazonbooks/pipelines.py
+++ amazonbooks/pipelines.py
@@ -18,12 +18,15 @@ class psqlpipeline(object):
     def process_item(self, item, spider):
         if (spider.name == "amazons") : #Only execute it for the amazons pipeline
             #print spider.name
-            conn = psycopg2.connect("dbname=amazon user=amazon password=amazon host=127.0.0.1")
-            cur = conn.cursor()
-            SQL = "INSERT INTO books (url,asin) VALUES (%s,%s);"
-            data = (item['url'], item['asin'])
-            cur.execute (SQL,data)
-            cur.execute (SQL,data)
+            try :
+                conn = psycopg2.connect("dbname=amazon user=amazon password=amazon host=127.0.0.1")
+                cur = conn.cursor()
+                SQL = "INSERT INTO books (url,asin) VALUES (%s,%s);"
+                data = (item['url'], item['asin'])
+                cur.execute (SQL,data)
+                conn.commit()
+            except:
+                print "Database error" 
             return item
         else:
             print item['url']
