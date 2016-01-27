5aeb381be9c925503f9d8bd4b47a116ec418799f
Brice <brice.jarmoszko@mail.mcgill.ca>
2016-01-27 00:08:23 -0500

    new table

--- amazonbooks/spiders/reviewer.py
+++ amazonbooks/spiders/reviewer.py
@@ -2,8 +2,8 @@
 import scrapy
 import psycopg2
 
-def getasinfromurl(url):
-    return url.split("/dp/")[1]
+def getrevieweridfromurl(url):
+    return url.split("/member-reviews/")[1]
 
 class ReviewerSpider(scrapy.Spider):
     name = "reviewers"
@@ -15,6 +15,8 @@ class ReviewerSpider(scrapy.Spider):
     rows = cur.fetchall()
     for row in rows:
         temp.append("http://www.amazon.com/gp/cdp/member-reviews/" + row[0].split("/profile/")[1])
+        cur.execute("""UPDATE reviewers SET reviewerid = %s  WHERE reviewerurl = %s)""",(row[0].split("/profile/")[1].strip(),row[0]))
+        conn.commit()
     start_urls = temp
     def parse(self, response):
         #item = HotRelease()
@@ -26,6 +28,13 @@ class ReviewerSpider(scrapy.Spider):
         except:
             pass
         #number of reviews
-        numberdiv = "".join(response.xpath("//b[contains(.,'Customer')]/../text()").extract()).encode("utf-8")
+        try :
+            numberdiv = "".join(response.xpath("//b[contains(.,'Customer')]/../text()").extract()).encode("utf-8")
+        except:
+            pass
         reviewsnumber = int(filter(str.isdigit, numberdiv))
+        reviewerid = getrevieweridfromurl(response.url)
         print topranking," ",helpfulvotes," ",reviewsnumber," ", response.url
+        #conn = psycopg2.connect("dbname=amazon user=amazon password=amazon host=127.0.0.1")
+        #cur = conn.cursor()
+        #SQL = "UPDATE reviewers SET topranking = %s, helpfulvotes = %s, reviewsnumber = %s, reviewerid = %s  WHERE revi"
